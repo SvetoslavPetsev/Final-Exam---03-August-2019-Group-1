5055ca3871240fb1615d6e154f94b038707b18c0
Svetoslav Petsev <svetoslav_petsev@users.noreply.github.com>
2020-03-19 22:33:53 +0200

    Add project files.

---
+++ Message Decrypter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace Message_Decrypter
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                string text = Console.ReadLine();
+
+                Regex pattern = new Regex(@"^([\$\%])(?<tag>[A-Z][a-z]{2,})\1\:\s\[(?<one>[0-9]+)\]\|\[(?<two>[0-9]+)\]\|\[(?<three>[0-9]+)\]\|$");
+
+                Match matchText = pattern.Match(text);
+
+                if (matchText.Success)
+                {
+                    string tag = matchText.Groups["tag"].Value;
+                    int symbolOne = int.Parse(matchText.Groups["one"].Value);
+                    int symbolTwo = int.Parse(matchText.Groups["two"].Value);
+                    int symbolThree = int.Parse(matchText.Groups["three"].Value);
+                    string decription = $"{(char)symbolOne}{(char)symbolTwo}{(char)symbolThree}";
+
+                    Console.WriteLine($"{tag}: {decription}");
+                }
+                else
+                {
+                    Console.WriteLine("Valid message not found!");
+                }
+            }
+        }
+    }
+}
---
+++ String Manipulator - Group 1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace String_Manipulator___Group_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "End")
+                {
+                    break;
+                }
+                string[] cmdArgs = input.Split();
+                string cmd = cmdArgs[0];
+                if (cmd == "Translate")
+                {
+                    char symbol = char.Parse(cmdArgs[1]);
+                    char replacment = char.Parse(cmdArgs[2]);
+                    text = text.Replace(symbol, replacment);
+                    Console.WriteLine(text);
+                }
+                else if (cmd == "Includes")
+                {
+                    string substr = cmdArgs[1];
+                    Console.WriteLine(text.Contains(substr));
+                }
+                else if (cmd == "Start")
+                {
+                    string substr = cmdArgs[1];
+                    if (substr.Length <= text.Length)
+                    {
+                        string startSubstr = text.Substring(0, substr.Length);
+                        Console.WriteLine(substr == startSubstr);
+                    }
+                }
+                else if (cmd == "Lowercase")
+                {
+                    text = text.ToLower();
+                    Console.WriteLine(text);
+                }
+                else if (cmd == "FindIndex")
+                {
+                    char symbol = char.Parse(cmdArgs[1]);
+                    int index = text.LastIndexOf(symbol);
+                    Console.WriteLine(index);
+                }
+                else if (cmd == "Remove")
+                {
+                    int startIndex = int.Parse(cmdArgs[1]);
+                    int count = int.Parse(cmdArgs[2]);
+
+                    if (startIndex >= 0 && startIndex + count <= text.Length)
+                    {
+                        string substr = text.Substring(startIndex, count);
+                        text = text.Replace(substr, string.Empty);
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+    }
+}
